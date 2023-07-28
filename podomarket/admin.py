e2d81b92e73cd41e37b663613135d1c1d45e3eba
Heo-JunHyeok <junhyeok_1996@naver.com>
2023-07-28 16:44:28 +0900

    프로필 사진 추가하기

--- podomarket/admin.py
+++ podomarket/admin.py
@@ -5,6 +5,6 @@ from .models import User, Post
 # Register your models here.
 admin.site.register(User, UserAdmin)
 UserAdmin.fieldsets += (
-    ("Custom Fields", {"fields": ("nickname", "kakao_id", "address")}),
+    ("Custom Fields", {"fields": ("nickname", "kakao_id", "address", "profile_pic")}),
 )
 admin.site.register(Post)
